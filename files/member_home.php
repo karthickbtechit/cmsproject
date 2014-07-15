8df857d9783e4f9d3b379e660f6f90305acc57c0
karthickbtechit <karthickbtechit@users.noreply.github.com>
2014-07-15 11:07:23 +0530

    member_home.php
    members home page., a member user can able to view this page as home page .

---
+++ files/member_home.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['user']))
+	{
+	 header('location:index.php');
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>member home page</title>
+<link href="styles/layout.css" rel="stylesheet" type="text/css" />
+<link href="menu/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!--Header Part Starts -->
+	<div id="header">
+	</div> <!-- id header ends-->
+<!--Header Part Ends -->
+<!--wapper Part Starts -->
+	<div id="wapper">
+	<h2>Welcome member </h2>
+		<?php 
+		include('menu.php');
+		?>
+	</div>
+<!--wapper Part Ends -->
+</body>
+</html>
